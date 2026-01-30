59785b1a4292c558d5f3fd425f7fb60ca5fb933d
Brandon Temple <bt1142@msstate.edu>
2026-01-30 03:02:36 -0600

    feat: Move showExampleTable function to virtualTable.js for improved modularity and organization

--- dragDrop.js
+++ dragDrop.js
@@ -1142,6 +1142,7 @@ function addDragAndDrop(table) {
 
 // Export the drag and drop system
 window.DragDropSystem = {
+  headerTrash,
   dragDropManager,
   addDragAndDrop,
   attachBubbleDropTarget,
